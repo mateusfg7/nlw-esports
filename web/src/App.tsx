9e188cd4cfc9aa6c03ed6cbb3657cd2c4eea8d85
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-09-13 13:46:41 -0300

    feat(web): create game cards

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -14,6 +14,55 @@ function App() {
         </span>{' '}
         está aqui.
       </h1>
+
+      <div className='grid grid-cols-6 gap-6 mt-16'>
+        <a href='' className='relative rounded-lg overflow-hidden'>
+          <img src='/game-1.png' alt='Game 1' />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>
+              League of Legends
+            </strong>
+            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
+          </div>
+        </a>
+        <a href='' className='relative rounded-lg overflow-hidden'>
+          <img src='/game-2.png' alt='Game 2' />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Dota 2</strong>
+            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
+          </div>
+        </a>
+        <a href='' className='relative rounded-lg overflow-hidden'>
+          <img src='/game-3.png' alt='Game 3' />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>CS:GO</strong>
+            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
+          </div>
+        </a>
+        <a href='' className='relative rounded-lg overflow-hidden'>
+          <img src='/game-4.png' alt='Game 4' />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Apex Legends</strong>
+            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
+          </div>
+        </a>
+        <a href='' className='relative rounded-lg overflow-hidden'>
+          <img src='/game-5.png' alt='Game 5' />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Fortnite</strong>
+            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
+          </div>
+        </a>
+        <a href='' className='relative rounded-lg overflow-hidden'>
+          <img src='/game-6.png' alt='Game 6' />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>
+              World of Warcraft
+            </strong>
+            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
+          </div>
+        </a>
+      </div>
     </div>
   )
 }
