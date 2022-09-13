fdd7fa36bfd7b07138fb9b2acc8996b90c8d98b1
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-09-13 13:33:09 -0300

    feat(web): add new background on tailwind extended theme
    - 'game-gradient'

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -7,6 +7,8 @@ module.exports = {
         galaxy: "url('/background-galaxy.png')",
         'nlw-gradient':
           'linear-gradient(89.86deg, #9572fc 23.08%, #47e7ad 33.94%, #e1d55d 44.57%)',
+        'game-gradient':
+          'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
       },
     },
   },
