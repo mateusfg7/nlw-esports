8510e606ab303b316740d83e59c3e96df7aced73
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-09-13 11:33:17 -0300

    feat(web): add logo image

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,13 @@
 import './styles/main.css'
 
+import logoImage from './assets/logo-nlw-esports.svg'
+
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <div className='max-w-[1344px] my-20 mx-auto flex flex-col items-center'>
+      <img src={logoImage} alt='Logo NLW eSports' />
+    </div>
+  )
 }
 
 export default App
