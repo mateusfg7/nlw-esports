818902a6082cfa54d947b14526f97104bb1e1268
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-09-13 11:33:59 -0300

    feat(web): add title

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,6 +6,14 @@ function App() {
   return (
     <div className='max-w-[1344px] my-20 mx-auto flex flex-col items-center'>
       <img src={logoImage} alt='Logo NLW eSports' />
+
+      <h1 className='text-6xl text-white font-black mt-20'>
+        Seu{' '}
+        <span className='bg-nlw-gradient bg-clip-text text-transparent'>
+          duo
+        </span>{' '}
+        está aqui.
+      </h1>
     </div>
   )
 }
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,6 +5,8 @@ module.exports = {
     extend: {
       backgroundImage: {
         galaxy: "url('/background-galaxy.png')",
+        'nlw-gradient':
+          'linear-gradient(89.86deg, #9572fc 23.08%, #47e7ad 33.94%, #e1d55d 44.57%)',
       },
     },
   },
