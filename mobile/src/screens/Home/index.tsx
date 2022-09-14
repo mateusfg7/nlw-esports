320ba217f6d1c7f235e04cafbbe7efb61e4dc480
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-09-14 11:34:20 -0300

    feat(mobile): add Heading component on Home

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,6 +1,7 @@
-import { Text, View, Image } from 'react-native'
+import { View, Image } from 'react-native'
 
 import logoImg from '../../assets/logo-nlw-esports.png'
+import { Heading } from '../../components/Heading'
 
 import { styles } from './styles'
 
@@ -8,6 +9,10 @@ export function Home() {
   return (
     <View style={styles.container}>
       <Image source={logoImg} style={styles.logo} />
+      <Heading
+        title='Encontre seu duo!'
+        subtitle='Selecione o game que deseja jogar...'
+      />
     </View>
   )
 }
