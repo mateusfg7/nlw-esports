01c6d1c60a3aad5fb837a5cf5c1d55a44ae01110
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-09-14 13:40:09 -0300

    feat(mobile): add GameCard on Home

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,8 +1,10 @@
-import { View, Image } from 'react-native'
+import { View, Image, FlatList } from 'react-native'
 
 import logoImg from '../../assets/logo-nlw-esports.png'
+import { GameCard } from '../../components/GameCard'
 import { Heading } from '../../components/Heading'
 
+import { GAMES } from '../../utils/games'
 import { styles } from './styles'
 
 export function Home() {
@@ -12,6 +14,14 @@ export function Home() {
       <Heading
         title='Encontre seu duo!'
         subtitle='Selecione o game que deseja jogar...'
+      />
+      <FlatList
+        data={GAMES}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <GameCard data={item} />}
+        contentContainerStyle={styles.contentList}
+        showsHorizontalScrollIndicator={false}
+        horizontal
       />
     </View>
   )
