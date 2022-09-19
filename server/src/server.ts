741bbee47854af3b821c12f82151bbd9654159c8
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-09-19 14:08:07 -0300

    feat(server): setup cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import { PrismaClient } from '@prisma/client'
 
 import { convertHourStringToMinutes } from './utils/convert-hour-string-to-minutes'
